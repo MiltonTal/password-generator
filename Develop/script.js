99707f6d93eacc8cf1bb0d339d73cec748781a51
MiltonTal <107081080+MiltonTal@users.noreply.github.com>
2022-07-30 12:42:28 -0500

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+//create the variables for the password
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+
+var characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?"];
+
+// Write password to the #password input
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+//create the prompt for the users input
+function generatePassword()
+
+
+//create the function logic for the password generator
