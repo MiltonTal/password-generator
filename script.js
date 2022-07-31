8647aa87f22fc6ff449e5c7391677baa90e00972
MiltonTal <107081080+MiltonTal@users.noreply.github.com>
2022-07-31 11:04:32 -0500

    edited function

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 //create the variables for the password
-// userLegnth = "";
-// userChoice = [];
+var passwordLength = [];
+var userChoice = [];
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?"];
 
-
-var confrimLetters;
+//Confrim variables
+var confrimLetters = letters;
 var confrimUpperLetters;
 var confrimNumbers;
 var confrimCharactors;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-// Write password to the #password input
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-//create the prompt for the users input
 
-
-
-      
+function generatePassword(){
+  passwordLength = prompt("Choose a password length between 8 and 128");
+  console.log(passwordLength);{
+    if (!passwordLength || passwordLength < 8 || passwordLength > 128)
+      generatePassword();
+  } alert("Your password will have " + passwordLength + " charactors");
   
+  confrimLetters = confrim("Would you like lower case letters in you password?");
+}
 
-//create the function logic for the password generator
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = password;
+}
