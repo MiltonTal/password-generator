5cfec75f1c379fd9d7e32ad974af621fdd9c3816
MiltonTal <107081080+MiltonTal@users.noreply.github.com>
2022-07-31 09:54:17 -0500

    added confrim var

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 //create the variables for the password
-userLegnth = 10;
-userChoice = [];
-letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?",
-];
+// userLegnth = "";
+// userChoice = [];
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?"];
 
 
+var confrimLetters;
+var confrimUpperLetters;
+var confrimNumbers;
+var confrimCharactors;
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -23,19 +26,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
 //create the prompt for the users input
-function generatePassword() {
 
-}
 
-function userPrompt(){
-  userLegnth = parseInt(prompt("Plseas choose how long you would like your password to be, between 8 and 128."));
-    if(isNaN(userLegnth) || userLegnth < 8 || userLegnth > 128){
-      alert("");
-      return false;
-    }
-    console.log(userLegnth)
-}
+
       
   
 
