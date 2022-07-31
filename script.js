d1c24990e73ae2a3aef0152b1643a9319771810a
MiltonTal <107081080+MiltonTal@users.noreply.github.com>
2022-07-31 00:30:56 -0500

    cleaned up & reset

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
+//create the variables for the password
+userLegnth = 10;
+userChoice = [];
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?",
+];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-//create confirm variable
-const userChoice;
-
-confirmIfNumbers
-
-confirmIfCharactors
-
-confirmLetters
-
-confirmLettersBig
-
-
-//create the variables for the password
-
-let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-let lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const characters = ["!","@","#","$","%","%","^","&","*","(",")","-","_","=","+","`","~","[","]","{","}","|",":",";","<",">","/","?",
-];
 
 // Write password to the #password input
 
@@ -34,12 +24,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 //create the prompt for the users input
-function generatePassword() {
-  userChoice = prompt(
-    "Choose your password legnth. Choose between 8 and 128 charactors."
-  );
-
-
-}
+function generatePassword() 
+      
+  
 
 //create the function logic for the password generator
